44820b83e01a10d76d24c0d6068ce152aea0dde8
unknown <xiadong@ZZYT.com>
2018-02-09 11:05:33 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <button v-on:click="getData()">Say what</button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods:{
+          getData(){
+            this.$http.get('http://localhost:8080/api/test').then(function(re){
+              alert(re.bodyText);
+              },function(){
+                  console.log("get email error")
+              })
+          }
+      }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
