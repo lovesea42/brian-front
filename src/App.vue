2c25efc4d01b858512d872f56364da2872a630b4
BrianXia <139652626@qq.com>
2018-02-09 14:27:04 +0800

    修改发布的端口

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ export default {
   name: 'App',
   methods:{
           getData(){
-            this.$http.get('http://localhost:8080/api/test').then(function(re){
+            this.$http.get('http://localhost:8001/api/test').then(function(re){
               alert(re.bodyText);
               },function(){
                   console.log("get email error")
